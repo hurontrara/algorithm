5e3511d755ede5ae7f9f0f945bcc370790018916
최선빈 <105468810+hurontrara@users.noreply.github.com>
2025-05-04 21:38:21 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 98.16 ms, Memory: 97.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,32 +1,37 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
     
-    static HashMap<String, Integer> hashMap = new HashMap<>();
+    HashMap<String, Integer> hashMap = new HashMap<>();
+    
     
     public String solution(String[] participant, String[] completion) {
         
-        for (int i = 0; i < participant.length; i++) {
-            hashMap.put(participant[i], hashMap.getOrDefault(participant[i], 0) + 1);
+        for (String human : participant) {
+            
+            hashMap.put(human, hashMap.getOrDefault(human, 0) + 1);
+            
         }
         
-        for (int i = 0; i < completion.length; i++) {
+        for (String human : completion) {
             
-            String person = completion[i];
-            int value = hashMap.get(person);
             
-            if (value == 1)
-                hashMap.remove(person);
-            else
-                hashMap.put(person, hashMap.get(person) - 1);
+            hashMap.put(human, hashMap.get(human) - 1);
+            
         }
         
-        for (String key : hashMap.keySet()) {
-            return key;
-        }        
+        for (String human : hashMap.keySet()) {
+            
+            if (hashMap.get(human) == 1)
+                return human;
+            
+        }
         
         return null;
         
+        
     }
+    
+    
+    
 }
