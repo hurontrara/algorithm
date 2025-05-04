57a452fa70289b0af0c197e4c3b28256a0023503
최선빈 <105468810+hurontrara@users.noreply.github.com>
2025-05-04 18:53:17 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,36 +1,39 @@
 import java.util.*;
 
+
 class Solution {
     
-    static int[] answer = new int[2];
+    int total; // 총 면적
     
     public int[] solution(int brown, int yellow) {
         
-        int total = brown + yellow;
+        total = brown + yellow;
         
-        // 세로 길이 기준
-        for (int i = 3; i <= total / 2; i++) {
+        for (int i = 1; i <= total; i++) {
             
-            // 가로 길이는
-            int j = total / i;
-            
-            if (total % i == 0 && ((i * 2) + (j * 2) - 4) == brown) {
+            if (total % i == 0) {
                 
-                answer[0] = j; answer[1] = i;
+                int row = total / i;
+                int col = i;
                 
-                return answer;
+                int sYellow = (row - 2) * (col - 2);
+                
+                if (sYellow == yellow) {
+                    
+                    return new int[]{row, col};
+                }
+                
                 
             }
+            
             
         }
         
         return null;
         
         
+        
     }
+    
+    
 }
-
-// brown + yellow = 전체 넓이
-// 가로 최소 3, 세로 최소 3
-// 넓이 12이고 가로 3일 때, 세로 4 이고, brown 갯수 구할 수 있음 : 가로 * 2 + 세로 * 2 - 4
-// 완전탐색 조져서 brown 갯수 같으면 ㄱ
